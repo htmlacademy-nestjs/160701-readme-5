2479fab90adda68c35eb5a48c0fbd558411f444c
Дмитрий <shcheklein.dev@gmail.com>
2024-01-06 20:46:45 +0300

    2.30. Детализирует ответы обработчиков маршрутов (@ApiResponse).

--- project/apps/users/src/app/authentication/authentication.controller.ts
+++ project/apps/users/src/app/authentication/authentication.controller.ts
@@ -1,15 +1,29 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { fillDto } from '@project/shared/helpers';
 import { UserRdo } from './rdo/user.rdo';
 import { LoginUserDto } from './dto/login-user.dto';
 import { LoggedUserRdo } from './rdo/logged-user.rdo';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('authentication')
 @Controller('auth')
 export class AuthenticationController {
   constructor(private readonly authService: AuthenticationService) {}
 
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The new user has been successfully created.',
+  })
   @Post('register')
   public async create(@Body() dto: CreateUserDto) {
     const newUser = await this.authService.register(dto);
@@ -17,6 +31,15 @@ export class AuthenticationController {
     return fillDto(UserRdo, newUser.toPOJO());
   }
 
+  @ApiResponse({
+    type: LoggedUserRdo,
+    status: HttpStatus.CREATED,
+    description: 'User has been successfully logged.',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Password or Login is wrong.',
+  })
   @Post('login')
   public async login(@Body() dto: LoginUserDto) {
     const verifiedUser = await this.authService.verifyUser(dto);
@@ -24,6 +47,16 @@ export class AuthenticationController {
     return fillDto(LoggedUserRdo, verifiedUser.toPOJO());
   }
 
+  @ApiResponse({
+    type: UserRdo,
+    status: HttpStatus.OK,
+    description: 'User found',
+  })
+  @ApiResponse({
+    type: NotFoundException, //TODO: не отображает структуру в swagger
+    status: HttpStatus.NOT_FOUND,
+    description: 'User not found',
+  })
   @Get(':id')
   public async show(@Param('id') id: string) {
     const existUser = await this.authService.getUser(id);
--- project/apps/users/src/app/authentication/rdo/user.rdo.ts
+++ project/apps/users/src/app/authentication/rdo/user.rdo.ts
@@ -1,18 +1,39 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 
 export class UserRdo {
   @Expose()
+  @ApiProperty({
+    description: 'The uniq user ID',
+    example: 'df191215-1f3c-407d-96b2-390bdfae1961',
+  })
   public id!: string;
 
   @Expose()
+  @ApiProperty({
+    description: 'User avatar path',
+    example: '/images/user.png',
+  })
   public avatar!: string;
 
   @Expose()
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@user.local',
+  })
   public email!: string;
 
   @Expose()
+  @ApiProperty({
+    description: 'User first name',
+    example: 'Keks',
+  })
   public firstname!: string;
 
   @Expose()
+  @ApiProperty({
+    description: 'User last name',
+    example: 'Keks',
+  })
   public lastname!: string;
 }
