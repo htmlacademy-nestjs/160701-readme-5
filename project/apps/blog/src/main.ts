141189b6ea394899a0c3bc596ba5b49230ec18fe
Дмитрий <shcheklein.dev@gmail.com>
2024-01-10 14:59:43 +0300

    feat: add blog service swagger

--- project/apps/blog/src/main.ts
+++ project/apps/blog/src/main.ts
@@ -5,14 +5,24 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = 'api/v1';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('The «Blog» service')
+    .setDescription('«Blog» service API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
+
+  const port = process.env.PORT || 4444;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
