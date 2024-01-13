443bfd0fc22f76bf263f0fbdb9d9a2189c8abb70
Дмитрий <shcheklein.dev@gmail.com>
2024-01-13 16:59:36 +0300

    feat: add posts res

--- project/apps/blog/src/app/app.module.ts
+++ project/apps/blog/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommentsModule } from './comments/comments.module';
 import { LikesModule } from './likes/likes.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [CommentsModule, LikesModule],
+  imports: [CommentsModule, LikesModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
