52edaf7153debb282606373e09f07ef58a29cc0b
Дмитрий <shcheklein.dev@gmail.com>
2024-01-14 19:56:18 +0300

    fix: remove blog app files

--- project/apps/blog/src/app/app.module.ts
+++ project/apps/blog/src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CommentsModule } from './comments/comments.module';
 import { LikesModule } from './likes/likes.module';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [CommentsModule, LikesModule, PostsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
