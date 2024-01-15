b9534899a5e41c04b072e2f58af32e0bb45894cd
Дмитрий <shcheklein.dev@gmail.com>
2024-01-15 18:18:38 +0300

    fix: post controller

--- project/apps/blog/src/app/posts/posts.controller.ts
+++ project/apps/blog/src/app/posts/posts.controller.ts
@@ -12,14 +12,37 @@ import { PostsService } from './posts.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { fillDto } from '@project/shared/helpers';
 import { PostTypesRdo } from './rdo/post-types.rdo';
+import { PostRdo } from './rdo/post.rdo';
+import { CreatePostDto } from './dto/create/create-post.dto';
+import { UpdatePostDto } from './dto/update/update-post.dto';
 
 @ApiTags('posts')
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @ApiResponse({
+    type: CreatePostDto,
+    status: HttpStatus.CREATED,
+    description: 'Post create successfully',
+  })
+  @ApiOperation({
+    summary: 'Создать пост',
+  })
+  @Post()
+  public async createVideo(@Body() createPostDto: CreatePostDto) {
+    const post = await this.postsService.create(createPostDto);
+
+    return fillDto(PostRdo, post);
+  }
+
   @ApiOperation({
     summary: 'Получить все посты',
+  })
+  @ApiResponse({
+    isArray: true,
+    type: PostRdo,
+    status: HttpStatus.OK,
   })
   @Get()
   public async findAll() {
@@ -40,6 +63,10 @@ export class PostsController {
     return fillDto(PostTypesRdo, { data: types });
   }
 
+  @ApiResponse({
+    type: PostRdo,
+    status: HttpStatus.OK,
+  })
   @ApiOperation({
     summary: 'Получить пост по id',
   })
@@ -48,11 +75,24 @@ export class PostsController {
     return this.postsService.findOne(id);
   }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-  //   return this.postsService.update(id, updatePostDto);
-  // }
+  @ApiResponse({
+    type: PostRdo,
+    status: HttpStatus.OK,
+  })
+  @ApiOperation({
+    summary: 'Обновить пост по id',
+  })
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+    return this.postsService.update(id, updatePostDto);
+  }
 
+  @ApiResponse({
+    status: HttpStatus.OK,
+  })
+  @ApiOperation({
+    summary: 'Удалить пост по id',
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.postsService.remove(id);
--- project/apps/blog/src/app/posts/posts.module.ts
+++ project/apps/blog/src/app/posts/posts.module.ts
@@ -3,11 +3,10 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { PostRepository } from './repository/post.repository';
 import { PostsContentModule } from './post-content/post-content.module';
-import { PostsCreateController } from './posts-create.controller';
 
 @Module({
   imports: [PostsContentModule],
-  controllers: [PostsCreateController, PostsController],
+  controllers: [PostsController],
   providers: [PostsService, PostRepository],
   exports: [PostRepository],
 })
