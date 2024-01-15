9c3d616020cd9ec3605486f4395b479de93549a8
Дмитрий <shcheklein.dev@gmail.com>
2024-01-15 18:13:43 +0300

    fix: remove content rdo & dto

--- project/apps/blog/src/app/posts/rdo/content/index.ts
+++
@@ -1,7 +0,0 @@
-import { LinkPostRdo } from './link-post.rdo';
-import { TextPostRdo } from './text-post.rdo';
-import { QuotePostRdo } from './quote-post.rdo';
-import { PhotoPostRdo } from './photo-post.rdo';
-import { VideoPostRdo } from './video-post.rdo';
-
-export { LinkPostRdo, TextPostRdo, QuotePostRdo, PhotoPostRdo, VideoPostRdo };
