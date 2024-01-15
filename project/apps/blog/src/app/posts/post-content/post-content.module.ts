04bd928ae7108e288464ba673be6d07c57cfe8ea
Дмитрий <shcheklein.dev@gmail.com>
2024-01-15 15:06:36 +0300

    fix: remove post-content controller

--- project/apps/blog/src/app/posts/post-content/post-content.module.ts
+++ project/apps/blog/src/app/posts/post-content/post-content.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PostContentController } from './post-content.controller';
 import { PostContentService } from './post-content.service';
 import { AllContentRepository } from '../repository/content';
 import { PostContentEntityFactory } from './post-content-entity.factory';
 import { PostContentRepositoryFactory } from './post-content-repository.factory';
 
 @Module({
-  controllers: [PostContentController],
   providers: [
     PostContentService,
     PostContentEntityFactory,
