f19acde2eedbd68f9a7d0c959ae14c8ba21f1fd4
Dmitry <shcheklein.dev@gmail.com>
2024-01-18 19:37:10 +0300

    5.40. Подключит JwtModule в AuthenticationModule. WIP

--- project/libs/shared/config/users/src/lib/jwt/get-jwt-options.ts
+++ project/libs/shared/config/users/src/lib/jwt/get-jwt-options.ts
@@ -1,6 +1,9 @@
 import { ConfigService } from '@nestjs/config';
+import { JwtModuleOptions } from '@nestjs/jwt';
 
-export async function getJwtOptions(configService: ConfigService) {
+export async function getJwtOptions(
+  configService: ConfigService
+): Promise<JwtModuleOptions> {
   return {
     secret: configService.get<string>('jwt.accessTokenSecret'),
     signOptions: {
