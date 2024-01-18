9e430b494a61d7e2461a81776a8e92ed7a218a7a
Dmitry <shcheklein.dev@gmail.com>
2024-01-18 20:47:49 +0300

    feat: add swagger

--- project/apps/file-vault/src/main.ts
+++ project/apps/file-vault/src/main.ts
@@ -7,11 +7,24 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { attachSwagger } from '@project/shared/helpers';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = 'api/v1';
   app.setGlobalPrefix(globalPrefix);
+
+  attachSwagger({
+    app,
+    DocumentBuilder: new DocumentBuilder()
+      .setTitle('The «File-vault» service')
+      .setDescription('«File-vault» service API')
+      .setVersion('1.0'),
+    swaggerCustomOptions: {
+      customSiteTitle: '[File-vault] Swagger UI',
+    },
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
