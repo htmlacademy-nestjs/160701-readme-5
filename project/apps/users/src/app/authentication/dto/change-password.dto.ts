aa39ee6917c881917234874f32592082d0783de8
Dmitry <shcheklein.dev@gmail.com>
2024-01-18 18:26:53 +0300

    5.10. Добавит базовую валидацию для DTO сервиса users.

--- project/apps/users/src/app/authentication/dto/change-password.dto.ts
+++ project/apps/users/src/app/authentication/dto/change-password.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { MaxLength, MinLength } from 'class-validator';
 
 export class ChangePasswordDto {
   @ApiProperty({
@@ -10,12 +11,20 @@ export class ChangePasswordDto {
   @ApiProperty({
     description: 'User old password',
     example: 'qwerty',
+    minLength: 6,
+    maxLength: 12,
   })
+  @MinLength(6)
+  @MaxLength(12)
   public oldPassword!: string;
 
   @ApiProperty({
     description: 'User new password',
     example: 'qwertynew',
+    minLength: 6,
+    maxLength: 12,
   })
+  @MinLength(6)
+  @MaxLength(12)
   public newPassword!: string;
 }
--- project/apps/users/src/app/authentication/dto/login-user.dto.ts
+++ project/apps/users/src/app/authentication/dto/login-user.dto.ts
@@ -1,15 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, MaxLength, MinLength } from 'class-validator';
+import { AUTH_USER_EMAIL_NOT_VALID } from '../authentication.constants';
 
 export class LoginUserDto {
   @ApiProperty({
     description: 'User email',
     example: 'user@user.ru',
   })
+  @IsEmail({}, { message: AUTH_USER_EMAIL_NOT_VALID })
   public email!: string;
 
   @ApiProperty({
     description: 'User password',
     example: '123456',
   })
+  @MinLength(6)
+  @MaxLength(12)
   public password!: string;
 }
