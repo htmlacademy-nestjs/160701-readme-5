d142475a4896a550af543f506ce14a3b9a5aa59d
Dmitry <shcheklein.dev@gmail.com>
2024-01-18 20:50:39 +0300

    feat: use ConfigService

--- project/apps/file-vault/src/main.ts
+++ project/apps/file-vault/src/main.ts
@@ -9,6 +9,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { attachSwagger } from '@project/shared/helpers';
 import { DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -25,7 +26,8 @@ async function bootstrap() {
       customSiteTitle: '[File-vault] Swagger UI',
     },
   });
-  const port = process.env.PORT || 3000;
+  const configService = app.get(ConfigService);
+  const port = configService.get('application.port');
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
