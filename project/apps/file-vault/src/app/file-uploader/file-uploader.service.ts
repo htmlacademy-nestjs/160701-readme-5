75869ea5e7a6c6a99df1599a7812ef46ba218288
Дмитрий <shcheklein.dev@gmail.com>
2024-01-19 17:43:09 +0300

    5.56. Добавит уникальные имена для файлов.

--- project/apps/file-vault/src/app/file-uploader/file-uploader.service.ts
+++ project/apps/file-vault/src/app/file-uploader/file-uploader.service.ts
@@ -6,6 +6,8 @@ import { writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 import { FileVaultConfig } from '@project/config/file-vault';
 import dayjs from 'dayjs';
+import { randomUUID } from 'node:crypto';
+import { extension } from 'mime-types';
 
 @Injectable()
 export class FileUploaderService {
@@ -18,7 +20,7 @@ export class FileUploaderService {
 
   private getUploadDirectoryPath(): string {
     const [year, month] = dayjs().format('YYYY MM').split(' ');
-    
+
     return join(this.config.uploadDirectory, year, month);
   }
 
@@ -29,7 +31,12 @@ export class FileUploaderService {
   public async saveFile(file: Express.Multer.File): Promise<string> {
     try {
       const uploadDirectoryPath = this.getUploadDirectoryPath();
-      const destinationFile = this.getDestinationFilePath(file.originalname);
+      const filename = randomUUID();
+      const fileExtension = extension(file.mimetype);
+
+      const destinationFile = this.getDestinationFilePath(
+        `${filename}.${fileExtension}`
+      );
 
       await ensureDir(uploadDirectoryPath);
       await writeFile(destinationFile, file.buffer);
@@ -41,4 +48,3 @@ export class FileUploaderService {
     }
   }
 }
-
