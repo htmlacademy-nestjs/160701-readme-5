5034543b77652481f963bd8d1b987ac2d9140346
Дмитрий <shcheklein.dev@gmail.com>
2024-01-20 18:18:20 +0300

    6.9. Подготовит основу для модуля EmailSubscriber. WIP.

--- project/apps/notify/src/app/app.module.ts
+++ project/apps/notify/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { NotifyConfigModule } from '@project/config/notify';
 import { MongooseModule } from '@nestjs/mongoose';
 import { getMongooseOptions } from '@project/shared/helpers';
+import { EmailSubscriberModule } from './email-subscriber/email-subscriber.module';
 
 @Module({
   imports: [
     NotifyConfigModule,
     MongooseModule.forRootAsync(getMongooseOptions('application.db')),
+    EmailSubscriberModule,
   ],
   controllers: [],
   providers: [],
