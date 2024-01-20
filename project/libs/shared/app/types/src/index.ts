b14fa574b3f7efee7bae0aab2c14582496d05350
Дмитрий <shcheklein.dev@gmail.com>
2024-01-20 13:12:36 +0300

    6.4. Добавит интерфейс Subscriber.

--- project/libs/shared/app/types/src/index.ts
+++ project/libs/shared/app/types/src/index.ts
@@ -16,3 +16,5 @@ export * from './lib/blog/posts/content/photo-post-content.interface';
 export * from './lib/blog/posts/content/quote-post-content.interface';
 export * from './lib/blog/posts/content/text-post-content.interface';
 export * from './lib/blog/posts/content/video-post-content.interface';
+
+export * from './lib/notify/subscriber.interface';
