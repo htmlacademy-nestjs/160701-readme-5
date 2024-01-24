9cc2a0cba4e7bc9e2aa10a978c9972762ceb39b5
Дмитрий <shcheklein.dev@gmail.com>
2024-01-24 17:58:12 +0300

    7.5. Добавит настройки для формирования refresh token

--- project/libs/shared/config/users/src/lib/jwt/jwt.config.ts
+++ project/libs/shared/config/users/src/lib/jwt/jwt.config.ts
@@ -6,6 +6,8 @@ async function getJwtConfig(): Promise<JwtConfiguration> {
   const config = plainToClass(JwtConfiguration, {
     accessTokenSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
     accessTokenExpiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRES_IN,
+    refreshTokenSecret: process.env.JWT_REFRESH_TOKEN_SECRET,
+    refreshTokenExpiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRES_IN,
   });
 
   await config.validate();
