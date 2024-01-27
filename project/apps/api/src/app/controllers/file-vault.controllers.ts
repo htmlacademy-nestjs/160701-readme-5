8610a5a1035749facb3e66296c94bbaeb57511d3
Дмитрий <shcheklein.dev@gmail.com>
2024-01-28 00:14:47 +0300

    feat: FileVaultController send file

--- project/apps/api/src/app/controllers/file-vault.controllers.ts
+++ project/apps/api/src/app/controllers/file-vault.controllers.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import 'multer';
+import { Controller, UploadedFile, UseFilters } from '@nestjs/common';
+import { AxiosExceptionFilter } from '../filters/axios-exception.filter';
+import { Post, UseGuards, UseInterceptors } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { ApplicationServiceURL } from '../app.config';
+import { CheckAuthGuard } from '../guards/check-auth.guard';
+import { UserIdInterceptor } from '../interceptors/userid.interceptor';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
+import FormData from 'form-data';
 
-@Controller('file-vault')
-export class FileVaultController {}
+@UseFilters(AxiosExceptionFilter)
+@Controller('files')
+export class FileVaultController {
+  constructor(private readonly httpService: HttpService) {}
+
+  //   @UseGuards(CheckAuthGuard)
+  @UseInterceptors(UserIdInterceptor)
+  @UseInterceptors(FileInterceptor('avatar'))
+  @Post('/upload/avatar')
+  public async create(
+    @UploadedFile()
+    file: Express.Multer.File
+  ) {
+    const form = new FormData();
+    form.append('avatar', file.buffer, {
+      filename: file.originalname,
+      contentType: file.mimetype,
+    });
+
+    const { data } = await this.httpService.axiosRef.postForm(
+      `${ApplicationServiceURL.FileVault}/upload/avatar`,
+      form
+    );
+
+    return data;
+  }
+}
