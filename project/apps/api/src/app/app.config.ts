26a2b74cf374b5c23e44639782843b0309dab8db
Дмитрий <shcheklein.dev@gmail.com>
2024-01-28 00:09:33 +0300

    fix: ApplicationServiceURL

--- project/apps/api/src/app/app.config.ts
+++ project/apps/api/src/app/app.config.ts
@@ -1,7 +1,7 @@
 export enum ApplicationServiceURL {
   Users = 'http://localhost:3333/api/v1/auth',
-  Blog = 'http://localhost:4444/api/v1/posts',
-  FileVault = 'http://localhost:5555/api/v1/files',
+  Blog = 'http://localhost:3666/api/v1/posts',
+  FileVault = 'http://localhost:3555/api/v1/files',
 }
 
 export const HTTP_CLIENT_MAX_REDIRECTS = 5;
