e26690168eb7f4ea7603e8633b5ab59687e22041
Дмитрий <shcheklein.dev@gmail.com>
2024-01-30 21:16:42 +0300

    fix: repositories

--- project/apps/blog/src/app/posts/repository/content/link-post.repository.ts
+++ project/apps/blog/src/app/posts/repository/content/link-post.repository.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { BaseMemoryRepository } from '@project/shared/core';
+import { BaseMongoRepository } from '@project/shared/core';
 import { LinkPostContentEntity } from '../../entities/content/link-post.entity';
+import { LinkPostContentModel } from '../../models/content/link-post.model';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class LinkPostRepository extends BaseMemoryRepository<LinkPostContentEntity> {}
+export class LinkPostRepository extends BaseMongoRepository<
+  LinkPostContentEntity,
+  LinkPostContentModel
+> {
+  constructor(
+    @InjectModel(LinkPostContentModel.name)
+    linkContentModel: Model<LinkPostContentModel>
+  ) {
+    super(linkContentModel, LinkPostContentEntity.fromObject);
+  }
+}
--- project/apps/blog/src/app/posts/repository/content/video-post.repository.ts
+++ project/apps/blog/src/app/posts/repository/content/video-post.repository.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { BaseMemoryRepository } from '@project/shared/core';
 import { VideoPostContentEntity } from '../../entities/content';
+import { BaseMongoRepository } from '@project/shared/core';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { VideoPostContentModel } from '../../models/content/video-post.model';
 
 @Injectable()
-export class VideoPostRepository extends BaseMemoryRepository<VideoPostContentEntity> {}
+export class VideoPostRepository extends BaseMongoRepository<
+  VideoPostContentEntity,
+  VideoPostContentModel
+> {
+  constructor(
+    @InjectModel(VideoPostContentModel.name)
+    videoContentModel: Model<VideoPostContentModel>
+  ) {
+    super(videoContentModel, VideoPostContentEntity.fromObject);
+  }
+}
