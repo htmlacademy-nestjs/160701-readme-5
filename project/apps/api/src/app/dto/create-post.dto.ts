7ea07067fdaade67b2142429834131bcada505ec
Дмитрий <shcheklein.dev@gmail.com>
2024-01-31 18:25:42 +0300

    fix: CreatePostApiDto

--- project/apps/api/src/app/dto/create-post.dto.ts
+++ project/apps/api/src/app/dto/create-post.dto.ts
@@ -1,4 +1,6 @@
 import { CreatePostDto } from '@project/dto';
 import { OmitType } from '@nestjs/swagger';
 
-export class CreatePostApiDto extends OmitType(CreatePostDto, ['author']) {}
+export class CreatePostApiDto extends OmitType(CreatePostDto, [
+  'author',
+] as const) {}
