9fa9da715fd2c43d64fb17586ad8e2c9dff98624
Дмитрий <shcheklein.dev@gmail.com>
2024-02-01 17:24:10 +0300

    fix: validation post content

--- project/libs/shared/app/types/src/lib/blog/posts/content/photo-post-content.interface.ts
+++ project/libs/shared/app/types/src/lib/blog/posts/content/photo-post-content.interface.ts
@@ -1,6 +1,7 @@
 import { Expose } from 'class-transformer';
 import { BasePostContent } from './post-content.interface';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class PhotoPostContent extends BasePostContent {
   @Expose()
@@ -8,5 +9,6 @@ export class PhotoPostContent extends BasePostContent {
     description: 'Photo image ID',
     example: '65b809b8d6443b043b33eedb',
   })
+  @IsString()
   imageId!: string;
 }
